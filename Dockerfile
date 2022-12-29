614913671926e0a282f2795f4459bcbce2dc4446
Tom <tslater366@gmail.com>
2022-12-29 19:01:16 +0000

    Docker File Fixes +
    - Fixed js mimetype error in python
    - Added stylesheet
    - Added charts js to project
    - Added basic chart to HTML
    - Fixed Dockerfile to work with ports + HTML structure

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@
 FROM python:3.9
 
 ADD main.py .
+ADD templates /templates
+ADD static /static
 
 RUN pip install flask
 
-CMD ["python", "./main.py"]
+CMD ["flask","--app","main","run","--host","0.0.0.0","-p","8080"]
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#windows registry mimetype fix
+import mimetypes
+mimetypes.add_type('application/javascript', '.js')
+mimetypes.add_type('text/css', '.css')
+
 from flask import Flask
 from flask import render_template
 
@@ -6,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def hello_world(name=None):
-    return render_template('index.html', name=name)
+	return render_template('index.html', name=name)
 
-if __name__ == '__main__':
-    app.run(debug=False,port=4996)
+# if __name__ == '__main__':
+# 	app.run(debug=True,port=4996)
