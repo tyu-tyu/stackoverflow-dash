dbdbbd51bd37c43fe95724ee2ece6cf89eb1b0ac
Tom <tslater366@gmail.com>
2023-02-18 23:30:30 +0000

    Nav Menu + Homepage
    - Added nav menu to layout template
    - Added icomoon svg icons to layout
    - Added dummy data to index and wrapper for chart
    - Adjusted stylesheet for mobile usable nav menu
    - Remove pi chart code from scripts, added animation for nav menu

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,40 +1,38 @@
-const ctx = document.getElementById('myChart');
-new Chart(ctx, {
-	type: 'bar',
-	data: {
-		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-		datasets: [{
-			label: '# of Votes',
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: ['Red','Blue','Yellow','Green','Purple','Orange'],
-			borderWidth: 1
-	  	}]
-	},
-	options: {
-		scales: {
-			y: {
-		  		beginAtZero: true
-			}
-	  	}
-	}
-});
-const piectx = document.getElementById('myPieChart');
-new Chart(piectx, {
-	type: 'pie',
-	data: {
-		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-		datasets: [{
-			label: '# of Votes',
-			data: [12, 19, 3, 5, 2, 3],
-			backgroundColor: ['Red','Blue','Yellow','Green','Purple','Orange'],
-			borderWidth: 1
-	  	}]
-	},
-	options: {
-		scales: {
-			y: {
-		  		beginAtZero: true
-			}
-	  	}
-	}
+document.addEventListener("DOMContentLoaded", function(event) {
+	document.getElementById('nav-btn-open').addEventListener('click', function() {
+		document.getElementById('side-nav-container').style.width = '6rem';
+		document.querySelector('body').style.marginLeft = '6rem';
+		document.querySelector('nav').style.opacity = 100;
+		document.getElementById('nav-btn-open').style.opacity = 0;
+	});
+	
+	document.getElementById('nav-btn-close').addEventListener('click', function() {
+		document.getElementById('side-nav-container').style.width = 0;
+		document.querySelector('body').style.marginLeft = 0;
+		document.querySelector('nav').style.opacity = 0;
+		document.getElementById('nav-btn-open').style.opacity = 100;
+	});
+	
+	const ctx = document.getElementById('myChart');
+	new Chart(ctx, {
+		type: 'bar',
+		responsive: true,
+		maintainAspectRatio: true,
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: ['Red','Blue','Yellow','Green','Purple','Orange'],
+				borderWidth: 1
+			  }]
+		},
+		options: {
+			scales: {
+				y: {
+					  beginAtZero: true
+				}
+			  }
+		}
+	});
 });
