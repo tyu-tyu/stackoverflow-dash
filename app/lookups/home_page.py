9a7b5a066833a1df9ec0ea700fc9770d5308518c
Tom <tslater366@gmail.com>
2023-02-27 18:57:26 +0000

    Index question doughnut
    - Added question doughnut chart to homepage
    - Changed all "" to '' in class file and routes
    - Added question doughnut chart function to route and class file

--- app/lookups/home_page.py
+++ app/lookups/home_page.py
@@ -5,60 +5,74 @@ class home_page:
 
 	def get_content_license(self):
 		response = {}
-		response["data"] = []
+		response['data'] = []
 		try:
-			self.cursor.execute("SELECT `content_license` FROM content_license")
+			self.cursor.execute('SELECT `content_license` FROM content_license')
 			result = self.cursor.fetchall()
 			for res in result:
-				response["data"].append(res[0])
-			response["status"] = True
+				response['data'].append(res[0])
+			response['status'] = True
 		except mariadb.Error as e:
-			response["data"].append(e)
-			response["success"] = False
+			response['data'].append(e)
+			response['success'] = False
 		return(response)
 	
 	def get_index_date_range(self):
 		response = {}
-		response["data"] = []
+		response['data'] = []
 		try:
-			self.cursor.callproc("get_index_date_range")
+			self.cursor.callproc('get_index_date_range')
 			result = self.cursor.fetchall()
 			for res in result:
-				response["data"].append(res[0])
-			response["status"] = True
+				response['data'].append(res[0])
+			response['status'] = True
 		except mariadb.Error as e:
-			response["data"].append(e)
-			response["success"] = False
+			response['data'].append(e)
+			response['success'] = False
 		return(response)
 	
 	def get_table_row_count(self):
 		response = {}
-		response["data"] = {}
+		response['data'] = {}
 		try:
-			self.cursor.callproc("get_table_row_count")
+			self.cursor.callproc('get_table_row_count')
 			result = self.cursor.fetchall()
 			for res in result:
-				response["data"][res[0]] = res[1]
-			response["data"]['posts'] = response["data"]["question"] + response["data"]["answer"]
-			response["status"] = True
+				response['data'][res[0]] = res[1]
+			response['data']['posts'] = response['data']['question'] + response['data']['answer']
+			response['status'] = True
 		except mariadb.Error as e:
-			response["data"].append(e)
-			response["success"] = False
+			response['data'].append(e)
+			response['success'] = False
 		return(response)
 	
 	def get_top_tags(self,limit):
 		response = {}
-		response["data"] = {}
-		response["data"]["tags"] = []
-		response["data"]["count"] = []
+		response['data'] = {}
+		response['data']['tags'] = []
+		response['data']['count'] = []
 		try:
-			self.cursor.callproc("get_top_tags",[limit,])
+			self.cursor.callproc('get_top_tags',[limit,])
 			result = self.cursor.fetchall()
 			for res in result:
-				response["data"]["tags"].append(res[0])
-				response["data"]["count"].append(res[1])
-			response["status"] = True
+				response['data']['tags'].append(res[0])
+				response['data']['count'].append(res[1])
+			response['status'] = True
 		except mariadb.Error as e:
-			response["data"].append(e)
-			response["success"] = False
+			response['data'].append(e)
+			response['success'] = False
+		return(response)
+
+	def get_index_question_details(self):
+		response = {}
+		response['data'] = []
+		try:
+			self.cursor.callproc('get_index_question_details')
+			result = self.cursor.fetchall()
+			for res in result:
+				response['data'].append(res[0])
+			response['status'] = True
+		except mariadb.Error as e:
+			response['data'].append(e)
+			response['success'] = False
 		return(response)
--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,11 @@ def index():
 	from app.lookups.home_page import home_page
 	result_data = {}
 	lookups = home_page(app.config['CURSOR'])
-	result_data["content_licenses"] = lookups.get_content_license()
-	result_data["question_date_range"] = lookups.get_index_date_range()
-	result_data["table_row_count"] = lookups.get_table_row_count()
-	result_data["top_tags"] = lookups.get_top_tags(10)
+	result_data['content_licenses'] = lookups.get_content_license()
+	result_data['question_date_range'] = lookups.get_index_date_range()
+	result_data['table_row_count'] = lookups.get_table_row_count()
+	result_data['top_tags'] = lookups.get_top_tags(10)
+	result_data['question_details'] = lookups.get_index_question_details()
 	return render_template('index.html.jinja', data=result_data)
 
 @app.route('/trending')
